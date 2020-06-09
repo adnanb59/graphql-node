2385a9413ac50fffc75aa0c9b7fe8b36bf233ec1
adnanb59 <adnan_bhuiyan@hotmail.com>
2020-06-09 00:24:03 -0400

    Finished

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,26 @@
 const feed = async (root, args, context, info) => {
-    let links = await context.prisma.link.findMany();
-    return links;
+    let conditions = args.filter ? {
+        OR: [
+            {
+                description: {
+                    contains: args.filter
+                }
+            }, {
+                title: {
+                    contains: args.filter
+                }
+            }
+        ]
+    } : {};
+    let links = await context.prisma.link.findMany({
+        where: conditions,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy
+    });
+    let count = await context.prisma.link.count({where: conditions});
+
+    return {links, count};
 };
 
 const link = async (root, args, context, info) => {
